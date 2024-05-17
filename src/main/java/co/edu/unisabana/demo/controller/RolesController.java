6de9d9c383fba9f16e0d1fb79bc1375f6c710b22
cylde <cyldecryo@gmail.com>
2024-05-16 23:41:24 -0500

    Adicion de Cross Origin para perfiles y rol

--- src/main/java/co/edu/unisabana/demo/controller/RolesController.java
+++ src/main/java/co/edu/unisabana/demo/controller/RolesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-
+@CrossOrigin("http://localhost:3000/")
 public class RolesController {
 
     private final RolesService service;
